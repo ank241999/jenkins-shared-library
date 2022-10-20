866a604daf566b2ba57c0e15fa2ad6c7fd6fba0e
ank241999 <100986553+ank241999@users.noreply.github.com>
2022-10-20 16:10:57 +0530

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,1 +1,43 @@
-
+def call(body) {
+  def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+  
+pipeline {
+    agent any
+    stages {
+        stage('Hello') {
+            steps {
+                echo 'Hello ankush jankins World......'
+            }
+        }
+        
+        stage('build') {
+            steps {
+                echo 'Hello ankush jankins World......'
+            }
+        }
+            
+        stage('deploy') {
+            steps {
+                echo 'deploy......'
+            }
+        }
+    
+        stage('test') {
+            steps {
+                echo 'testing......'
+            }
+        }
+    
+        stage('release') {
+            steps {
+                echo 'releasing......'
+            }
+        }
+    }
+}  
+  
+ 
+}
